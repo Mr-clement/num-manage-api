36f592599a7ce1a1a3dc83a4e84e38436c0988ca
MrCLET <amlaganclement@gmail.com>
2026-06-16 16:59:07 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const app = express();
+const PORT = 5000 
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
